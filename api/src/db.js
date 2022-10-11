398a20b3989156de86cc70d10559f1ad88300964
Cesar <cesar.alcoholado@hotmail.com>
2022-10-11 11:26:57 -0300

    added import pg

--- api/src/db.js
+++ api/src/db.js
@@ -2,14 +2,20 @@ require('dotenv').config();
 const { Sequelize } = require('sequelize');
 const fs = require('fs');
 const path = require('path');
+import pg from "pg";
+
 const {
   DB_USER, DB_PASSWORD, DB_HOST,
 } = process.env;
 
-const sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`, {
-  logging: false, // set to console.log to see the raw SQL queries
-  native: false, // lets Sequelize know we can use pg-native for ~30% more speed
-});
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/food`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+    dialectModule: pg,
+  }
+);
 const basename = path.basename(__filename);
 
 const modelDefiners = [];
