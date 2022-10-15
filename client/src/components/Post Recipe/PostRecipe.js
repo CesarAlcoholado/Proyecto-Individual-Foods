112452b9257c2a18be186b6e6775ee1e798f8443
Cesar <cesar.alcoholado@hotmail.com>
2022-10-15 18:43:42 -0300

    update post recipe validation

--- client/src/components/Post Recipe/PostRecipe.js
+++ client/src/components/Post Recipe/PostRecipe.js
@@ -66,7 +66,7 @@ export default function PostRecipe() {
 
   function onInputChange(e) {
     e.preventDefault();
-    const pattern = new RegExp("/^[A-Za-z\s]*$/", "i");
+    const pattern = new RegExp("^[A-Z]", "i");
     if (e.target.name === "healthscore") {
       if (e.target.value < 0 || e.target.value > 100) {
         alert("healthscore must be higher than 0 and lower than 100");
